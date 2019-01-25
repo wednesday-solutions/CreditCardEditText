0f13070992fc1b491127eb3eda5d0176c14f5b1f
Sujay <sujay.ka@gmail.com>
2019-01-25 08:51:12 +0530

    rupay & diners supprt

--- library/src/main/java/com/alihafizji/library/CreditCardPatterns.java
+++ library/src/main/java/com/alihafizji/library/CreditCardPatterns.java
@@ -23,6 +23,8 @@ public class CreditCardPatterns implements CreditCardEditText.CreditCartEditText
         CreditCardEditText.CreditCard visa = new CreditCardEditText.CreditCard("^4[0-9]{12}(?:[0-9]{3})?$", mContext.getResources().getDrawable(R.drawable.visa), CreditCardTypeEnum.VISA.cartType);
         CreditCardEditText.CreditCard mastercard = new CreditCardEditText.CreditCard("^5[1-5][0-9]{14}$", mContext.getResources().getDrawable(R.drawable.mastercard), CreditCardTypeEnum.MASTER_CARD.cartType);
         CreditCardEditText.CreditCard amex = new CreditCardEditText.CreditCard("^3[47][0-9]{13}$", mContext.getResources().getDrawable(R.drawable.amex), CreditCardTypeEnum.AMERICAN_EXPRESS.cartType);
+        CreditCardEditText.CreditCard diners = new CreditCardEditText.CreditCard("^3(?:0[0-5]|[68][0-9])[0-9]{4,}$", mContext.getResources().getDrawable(R.drawable.icons_diners_club), CreditCardTypeEnum.DINERS_CLUB.cartType);
+        CreditCardEditText.CreditCard rupay = new CreditCardEditText.CreditCard("^6[0-9]{15}$", mContext.getResources().getDrawable(R.drawable.icons_rupay), CreditCardTypeEnum.RUPAY_CARD.cartType);
 
         listOfPatterns.add(visa);
         listOfPatterns.add(mastercard);
